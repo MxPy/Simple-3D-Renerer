3ad44bae9eeef971ce05e35f121056463f5fde20
Maximilian Patalas <maksymilianpatalas@gmail.com>
2024-01-13 19:07:53 +0100

    add clipping

--- dem3.c
+++ dem3.c
@@ -206,6 +206,123 @@ uint32_t clacRGB(int R, int G, int B){
     return result;
 }
 
+vec3d Vector_IntersectPlane(vec3d *plane_p, vec3d *plane_n, vec3d *lineStart, vec3d *lineEnd) {
+    *plane_n = Vector_Normalise(plane_n);
+    float plane_d = -Vector_DotProduct(plane_n, plane_p);
+    float ad = Vector_DotProduct(lineStart, plane_n);
+    float bd = Vector_DotProduct(lineEnd, plane_n);
+    float t = (-plane_d - ad) / (bd - ad);
+    vec3d lineStartToEnd = Vector_Sub(lineEnd, lineStart);
+    vec3d lineToIntersect = Vector_Mul(&lineStartToEnd, t);
+    return Vector_Add(lineStart, &lineToIntersect);
+}
+
+float dist(vec3d *p, vec3d *plane_n, vec3d *plane_p) {
+    vec3d n = Vector_Normalise(plane_n);
+    return (n.x * p->x + n.y * p->y + n.z * p->z - Vector_DotProduct(&n, plane_p));
+}
+
+int Triangle_ClipAgainstPlane(vec3d plane_p, vec3d plane_n, triangle *in_tri, triangle *out_tri1, triangle *out_tri2) {
+    // Make sure plane normal is indeed normal
+    plane_n = Vector_Normalise(&plane_n);
+
+
+    // Create two temporary storage arrays to classify points either side of the plane
+    // If distance sign is positive, the point lies on the "inside" of the plane
+    vec3d *inside_points[3];
+    int nInsidePointCount = 0;
+    vec3d *outside_points[3];
+    int nOutsidePointCount = 0;
+
+    // Get signed distance of each point in the triangle to the plane
+    float d0 = dist(&in_tri->p[0], &plane_n, &plane_p);
+    float d1 = dist(&in_tri->p[1], &plane_n, &plane_p);
+    float d2 = dist(&in_tri->p[2], &plane_n, &plane_p);
+
+    if (d0 >= 0) {
+        inside_points[nInsidePointCount++] = &in_tri->p[0];
+    } else {
+        outside_points[nOutsidePointCount++] = &in_tri->p[0];
+    }
+    if (d1 >= 0) {
+        inside_points[nInsidePointCount++] = &in_tri->p[1];
+    } else {
+        outside_points[nOutsidePointCount++] = &in_tri->p[1];
+    }
+    if (d2 >= 0) {
+        inside_points[nInsidePointCount++] = &in_tri->p[2];
+    } else {
+        outside_points[nOutsidePointCount++] = &in_tri->p[2];
+    }
+
+    // Now classify triangle points and break the input triangle into
+    // smaller output triangles if required. There are four possible
+    // outcomes...
+
+    if (nInsidePointCount == 0) {
+        // All points lie on the outside of the plane, so clip the whole triangle
+        // It ceases to exist
+        return 0; // No returned triangles are valid
+    }
+
+    if (nInsidePointCount == 3) {
+        // All points lie on the inside of the plane, so do nothing
+        // and allow the triangle to simply pass through
+        *out_tri1 = *in_tri;
+        return 1; // Just the one returned original triangle is valid
+    }
+
+    if (nInsidePointCount == 1 && nOutsidePointCount == 2) {
+        // Triangle should be clipped. As two points lie outside
+        // the plane, the triangle simply becomes a smaller triangle
+
+        // Copy appearance info to the new triangle
+        //out_tri1->col = in_tri->col;
+        //out_tri1->sym = in_tri->sym;
+
+        // The inside point is valid, so keep that...
+        out_tri1->p[0] = *inside_points[0];
+
+        // But the two new points are at the locations where the 
+        // original sides of the triangle (lines) intersect with the plane
+        out_tri1->p[1] = Vector_IntersectPlane(&plane_p, &plane_n, inside_points[0], outside_points[0]);
+        out_tri1->p[2] = Vector_IntersectPlane(&plane_p, &plane_n, inside_points[0], outside_points[1]);
+
+        return 1; // Return the newly formed single triangle
+    }
+
+    if (nInsidePointCount == 2 && nOutsidePointCount == 1) {
+        // Triangle should be clipped. As two points lie inside the plane,
+        // the clipped triangle becomes a "quad". Fortunately, we can
+        // represent a quad with two new triangles
+
+        // Copy appearance info to new triangles
+        //out_tri1->col = in_tri->col;
+        //out_tri1->sym = in_tri->sym;
+
+        //out_tri2->col = in_tri->col;
+        //out_tri2->sym = in_tri->sym;
+
+        // The first triangle consists of the two inside points and a new
+        // point determined by the location where one side of the triangle
+        // intersects with the plane
+        out_tri1->p[0] = *inside_points[0];
+        out_tri1->p[1] = *inside_points[1];
+        out_tri1->p[2] = Vector_IntersectPlane(&plane_p, &plane_n, inside_points[0], outside_points[0]);
+
+        // The second triangle is composed of one of the inside points, a
+        // new point determined by the intersection of the other side of the 
+        // triangle and the plane, and the newly created point above
+        out_tri2->p[0] = *inside_points[1];
+        out_tri2->p[1] = out_tri1->p[2];
+        out_tri2->p[2] = Vector_IntersectPlane(&plane_p, &plane_n, inside_points[1], outside_points[0]);
+
+        return 2; // Return
+    }
+}
+
+
+
 void createCube(mesh* meshCube, float x, float y, float z, float size) {
     // SOUTH
     meshCube->tris[meshCube->counter++] = (struct triangle){{x, y, z, 1, x, y + size, z, 1, x + size, y + size, z, 1}};
@@ -306,9 +423,9 @@ float yaw = 0;
 while(true){
     
     if(fpsI == fps){
-        vec3d vForward = Vector_Mul(&vLookDir, 4.0f * fElapsedTime);
-        if(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_7) == GPIO_PIN_7){ yaw -= 2.0f * fElapsedTime;}
-        if(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_6) == GPIO_PIN_6){ yaw += 2.0f * fElapsedTime;}
+        vec3d vForward = Vector_Mul(&vLookDir, 2.0f * fElapsedTime);
+        if(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_7) == GPIO_PIN_7){ yaw += 2.0f * fElapsedTime;}
+        if(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_6) == GPIO_PIN_6){ yaw -= 2.0f * fElapsedTime;}
         if(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_5) == GPIO_PIN_5){ vCamera = Vector_Add(&vCamera, &vForward);}
         if(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_4) == GPIO_PIN_4){ vCamera = Vector_Sub(&vCamera, &vForward);}
         if(GPIOPinRead(GPIO_PORTJ_BASE,GPIO_PIN_3) == GPIO_PIN_3){ vCamera.y += 1.0f * fElapsedTime;}
@@ -419,9 +536,15 @@ while(true){
                     MultiplyMatrixVector(&triTranslated.p[1], &triViewd.p[1], &matView);
                     MultiplyMatrixVector(&triTranslated.p[2], &triViewd.p[2], &matView);
 
-                    MultiplyMatrixVector(&triViewd.p[0], &triProjected.p[0], &matProj);
-                    MultiplyMatrixVector(&triViewd.p[1], &triProjected.p[1], &matProj);
-                    MultiplyMatrixVector(&triViewd.p[2], &triProjected.p[2], &matProj);
+                    int nClippedTriangles = 0;
+                    triangle clipped[2];
+                    nClippedTriangles = Triangle_ClipAgainstPlane((vec3d){ 0.0f, 0.0f, 0.1f }, (vec3d){ 0.0f, 0.0f, 1.0f }, &triViewd, &clipped[0], &clipped[1]);
+                    for (int n = 0; n < nClippedTriangles; n++)
+				{
+                    MultiplyMatrixVector(&clipped->p[0], &triProjected.p[0], &matProj);
+                    MultiplyMatrixVector(&clipped->p[1], &triProjected.p[1], &matProj);
+                    MultiplyMatrixVector(&clipped->p[2], &triProjected.p[2], &matProj);
+                    
 
                     // Scale into view
                     triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
@@ -438,6 +561,7 @@ while(true){
                     DrawTriangle(&sContext, triProjected.p[0].x, triProjected.p[0].y,
                         triProjected.p[1].x, triProjected.p[1].y,
                         triProjected.p[2].x, triProjected.p[2].y);
+                }
                 }
 
             }
